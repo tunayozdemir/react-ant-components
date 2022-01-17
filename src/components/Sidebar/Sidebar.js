aacc87b9759b8d52e75e3f02c734ba175712917c
Tunay Özdemir <tunay.ozdemir@consultant.turkcell.com.tr>
2022-01-18 00:57:26 +0300

    sidebar revizyon

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Layout, Avatar, Menu, Breadcrumb } from 'antd';
-import { AppstoreOutlined, SettingOutlined, MailOutlined } from '@ant-design/icons'
+import { TableOutlined, EnterOutlined } from '@ant-design/icons'
 // import { Link } from 'react-router-dom';
 // import routes from '../../Route';
 import AvatarLion from '../../assets/icons/lion.png'
@@ -32,29 +32,19 @@ function Sidebar() {
             defaultOpenKeys={['sub1']}
             mode="inline"
           >
-            <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-              <Menu.ItemGroup key="g1" title="Item 1">
-                <Menu.Item key="1">Option 1</Menu.Item>
-                <Menu.Item key="2">Option 2</Menu.Item>
-              </Menu.ItemGroup>
-              <Menu.ItemGroup key="g2" title="Item 2">
-                <Menu.Item key="3">Option 3</Menu.Item>
-                <Menu.Item key="4">Option 4</Menu.Item>
+            <SubMenu key="sub1" icon={<TableOutlined />} title="Tables Componntes">
+              <Menu.ItemGroup key="g1" title="Tables">
+                <Menu.Item key="1">Tablo 1</Menu.Item>
+                <Menu.Item key="2">Tablo 2</Menu.Item>
               </Menu.ItemGroup>
             </SubMenu>
-            <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+            <SubMenu key="sub2" icon={<EnterOutlined />} title="Inputs Compontnes">
               <Menu.Item key="5">Option 5</Menu.Item>
               <Menu.Item key="6">Option 6</Menu.Item>
               <SubMenu key="sub3" title="Submenu">
                 <Menu.Item key="7">Option 7</Menu.Item>
                 <Menu.Item key="8">Option 8</Menu.Item>
               </SubMenu>
-            </SubMenu>
-            <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
